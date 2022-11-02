d06d6a63f257c349d59c6958d14bf486be0c321d
jubilee <jsarma166@gmail.com>
2022-11-02 13:52:17 +0530

    C++

---
+++ tut8_arrays.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int n){
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[n];
+    }
+    
+}
+
+int main()
+{
+
+    // int arr[5] = {1, 3, 4, 6, 7};
+    // cout<<arr[0]<<endl;
+
+    // int n;
+    // cin >> n;
+    // int a[n];
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     a[n] = {1};
+    //     cout << a[n] << endl;
+    // }
+
+    return 0;
+}
